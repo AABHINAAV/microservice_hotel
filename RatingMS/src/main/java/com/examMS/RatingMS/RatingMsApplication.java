191784ace223761430e72847c9b062a771cdc550
aabhinaav <abhinavgargchand@gmail.com>
2023-02-26 22:44:44 +0530

    service registry done for rating ms

--- RatingMS/src/main/java/com/examMS/RatingMS/RatingMsApplication.java
+++ RatingMS/src/main/java/com/examMS/RatingMS/RatingMsApplication.java
@@ -3,8 +3,10 @@ package com.examMS.RatingMS;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingMsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
@@ -13,6 +15,6 @@ public class RatingMsApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Starting the code");
+		System.out.println("Rating MS Started");
 	}
 }
