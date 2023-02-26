076e898dd1122c0f0706a4280c25325f3a54ffc5
aabhinaav <abhinavgargchand@gmail.com>
2023-02-26 22:35:27 +0530

    service registry done for Hotel ms

--- HotelMS/src/main/java/com/examMS/HotelMS/HotelMsApplication.java
+++ HotelMS/src/main/java/com/examMS/HotelMS/HotelMsApplication.java
@@ -1,13 +1,20 @@
 package com.examMS.HotelMS;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class HotelMsApplication {
+@EnableEurekaClient
+public class HotelMsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelMsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hotel MS Started");
+	}
 }
