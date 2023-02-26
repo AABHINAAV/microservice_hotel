ba77969c706234beeccc9737bbb5e974a703ab95
aabhinaav <abhinavgargchand@gmail.com>
2023-02-26 22:13:34 +0530

    port of service registry changed to default port 8761
    EnableEurekaClient annotation added and configurations added in user microservice

--- UserMS/src/main/java/com/examMS/UserMS/UserMsApplication.java
+++ UserMS/src/main/java/com/examMS/UserMS/UserMsApplication.java
@@ -1,15 +1,20 @@
 package com.examMS.UserMS;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class UserMsApplication {
+public class UserMsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserMsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("User ms started");
+	}
 }
