42016ae7c86dfaab94d469b2523542287bbed00c
aabhinaav <abhinavgargchand@gmail.com>
2023-04-07 22:39:30 +0530

    @EnableEurekaClient added in entrance file port number, application name, eureka client details added api gateway ports added

--- ApiGateWay/src/main/java/com/examMS/ApiGateway/ApiGateWayApplication.java
+++ ApiGateWay/src/main/java/com/examMS/ApiGateway/ApiGateWayApplication.java
@@ -1,13 +1,20 @@
 package com.examMS.ApiGateway;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class ApiGateWayApplication {
+@EnableEurekaClient
+public class ApiGateWayApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGateWayApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Api Gateway started");
+	}
 }
