a221d382850fee69802a9cd04b3f34497b7a475c
aabhinaav <abhinavgargchand@gmail.com>
2023-04-07 17:36:35 +0530

    added openFeint client dependency and added @EnableFeignClient annotation to UserMSApplication.java

--- UserMS/src/main/java/com/examMS/UserMS/UserMsApplication.java
+++ UserMS/src/main/java/com/examMS/UserMS/UserMsApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class UserMsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
