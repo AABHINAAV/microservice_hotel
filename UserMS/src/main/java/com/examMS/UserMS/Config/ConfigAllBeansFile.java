a532aa4b22e48a78992afc6ab95f5764bae614a4
aabhinaav <abhinavgargchand@gmail.com>
2023-04-12 10:57:29 +0530

    created bean of OAuth2AuthorizedClientManager

--- UserMS/src/main/java/com/examMS/UserMS/Config/ConfigAllBeansFile.java
+++ UserMS/src/main/java/com/examMS/UserMS/Config/ConfigAllBeansFile.java
@@ -3,6 +3,12 @@ package com.examMS.UserMS.Config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -12,5 +18,26 @@ public class ConfigAllBeansFile {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository
+    ) {
+//        oAuth2AuthorizedClientManager is an interface so we need to make a class then return object
+//        of that class
+        DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, oAuth2AuthorizedClientRepository
+        );
+
+        OAuth2AuthorizedClientProvider oAuth2AuthorizedClientProvider = OAuth2AuthorizedClientProviderBuilder
+                .builder()
+                .clientCredentials()
+                .build();
+
+        defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(oAuth2AuthorizedClientProvider);
+
+        return defaultOAuth2AuthorizedClientManager;
     }
 }
